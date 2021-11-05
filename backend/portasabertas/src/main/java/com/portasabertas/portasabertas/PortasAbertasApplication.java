fd36e6a2242106f5243d641d4c2deef46d29da87
Kelvin Murilo <kelvinmuriloas@gmail.com>
2021-11-05 20:24:49 -0300

    criando projeto backend

---
+++ backend/portasabertas/src/main/java/com/portasabertas/portasabertas/PortasAbertasApplication.java
@@ -0,0 +1,13 @@
+package com.portasabertas.portasabertas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortasAbertasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortasAbertasApplication.class, args);
+	}
+
+}
