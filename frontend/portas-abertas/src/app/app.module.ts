a5d730a9cc5adaf2e2758d0a1c01c4ebdf0bdec2
Cassio Dos Santos <cassioms764@gmail.com>
2021-11-11 10:00:13 -0300

    Implementação de rotas

--- frontend/portas-abertas/src/app/app.module.ts
+++ frontend/portas-abertas/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './features/login/login.component';
+import { CadastroComponent } from './features/cadastro/cadastro.component';
+import { AppRoutingModule } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
