5cd35a6110f49fee299315420d73ecabd09dcb29
Cassio Dos Santos <cassioms764@gmail.com>
2021-11-25 11:47:44 -0300

    implementendo tela de cadastro

--- frontend/portas-abertas/src/app/features/cadastro/cadastro.component.ts
+++ frontend/portas-abertas/src/app/features/cadastro/cadastro.component.ts
@@ -13,12 +13,53 @@ export class CadastroComponent implements OnInit {
   }
 
   test(){
-    const slidePage = document.querySelector(".slidepage");
+    //Paginas
+    const pageOne = document.querySelector(".pageOne");
+    const pageTwo = document.querySelector(".pageTwo");
+    const pageThree = document.querySelector(".pageThree");
+    const pageFour = document.querySelector(".pageFour");
+    //Buton Inicial
     const firtNextBtn = document.querySelector(".nextBtn");
-  
+
     firtNextBtn.addEventListener("click", function(){
-    slidePage.setAttribute("style", "margin-left:-25%;")
-  });
+      pageOne.setAttribute("style", "visibility: hidden;")
+      pageTwo.setAttribute("style", "visibility: visible;")
+    });
+    //button q2
+    const yes = document.querySelector(".yes");
+    const nao = document.querySelector(".no");
+
+    yes.addEventListener("click", function(){
+      pageTwo.setAttribute("style", "visibility: hidden;");
+      pageThree.setAttribute("style", "visibility: visible;");
+    })
+    //button q3
+    const backInfo = document.querySelector(".backInfo");
+    const nextInfo = document.querySelector(".nextInfo");
+
+    backInfo.addEventListener("click", function(){
+      pageThree.setAttribute("style", "visibility: hidden;");
+      pageTwo.setAttribute("style", "visibility: visible;");
+    });
+
+    nextInfo.addEventListener("click", function(){
+      pageThree.setAttribute("style", "visibility: hidden;");
+      pageFour.setAttribute("style", "visibility: visible;");
+    });
+    //button q4
+    const backEnd = document.querySelector(".backEnd");
+    const nextEnd = document.querySelector(".nextEnd");
+
+    backEnd.addEventListener("click", function(){
+      pageFour.setAttribute("style", "visibility: hidden;");
+      pageThree.setAttribute("style", "visibility: visible;");
+    });
+  
+    
+
+    
+
+
   }
 
 }
