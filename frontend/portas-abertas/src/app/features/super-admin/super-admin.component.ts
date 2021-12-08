d564e1fb8bbc234736b89bd6a06128bdc2768470
Cassio Dos Santos <cassioms764@gmail.com>
2021-12-08 09:47:43 -0300

    implementando tabela

--- frontend/portas-abertas/src/app/features/super-admin/super-admin.component.ts
+++ frontend/portas-abertas/src/app/features/super-admin/super-admin.component.ts
@@ -1,32 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/core/user/user.service';
+declare const $;
 
 @Component({
   selector: 'app-super-admin',
   templateUrl: './super-admin.component.html',
   styleUrls: ['./super-admin.component.scss'],
 })
-export class SuperAdminComponent implements OnInit {
+export class SuperAdminComponent {
 
   Name: string = "Cássio"
 
-  //public dados = [{name: 'Cassio', tel: '819999999', dia: 'Segunda', horario: '8:00'}];
-  //dtOptions: DataTables.Settings = {};
-
-
   constructor(private userService: UserService, private router: Router) {}
-
-  ngOnInit(): void {
-    /*this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 5,
-      lengthMenu : [5, 10, 15],
-      processing: true
-    }*/
-    this.exec();
+  ngAfterViewInit(): void {
 
   }
+
+  ngOnInit(): void {
+    this.exec();
+    $('example').DataTable();
+  }
+
 
   logout(): void {
     this.userService.logout();
